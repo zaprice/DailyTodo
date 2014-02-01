1734d80dbc33eb965e3b9a2571a23c7d682a9a93
zaprice <zprice11@gmail.com>
2014-02-01 14:45:59 -0500

    Added comments

--- src/com/zaprice/dailytodo/AddTaskActivity.java
+++ src/com/zaprice/dailytodo/AddTaskActivity.java
@@ -13,17 +13,18 @@ public class AddTaskActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add);
-		
+		//UI elements; EditText lets the user type in a name for their task
 		final EditText taskName = (EditText) findViewById(R.id.editText1);
-		
 		final Button addTask = (Button) findViewById(R.id.button1);
         addTask.setOnClickListener(new View.OnClickListener() {
+        	//Listener used to detect when a user is finished specifying their task
             public void onClick(View v) {
-                // Perform action on click
             	if(taskName.getText().toString() == "") {
+            		//Cancels adding a task if the name is empty
             		setResult(RESULT_CANCELED);
                 	finish();
             	}
+            	//Sends task name back to MainActivity by calling back to onActivityResult
             	getIntent().putExtra("task name", taskName.getText().toString());
             	setResult(RESULT_OK, getIntent());
             	finish();
@@ -33,6 +34,7 @@ public class AddTaskActivity extends Activity {
 	
 	@Override
 	public void onBackPressed() {
+		//Cancels adding a task and finishes the activity
 		setResult(RESULT_CANCELED);
 		super.onBackPressed();
 	}
--- src/com/zaprice/dailytodo/MainActivity.java
+++ src/com/zaprice/dailytodo/MainActivity.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Paint;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -28,6 +31,7 @@ public class MainActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//Called when the app is first started; next call in the lifecycle is onStart
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		tasks = new ArrayList<String>();
@@ -35,42 +39,45 @@ public class MainActivity extends Activity {
 		taskList = (ListView) findViewById(R.id.taskList);
 		taskListAdapter = new ArrayAdapter<String>(this, R.layout.list_item);
 		taskList.setAdapter(taskListAdapter);
+		
+		taskList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			//Listener used to detect when user touches a task
+			//On touch, a task will be marked done and given a strikethrough decoration
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				Log.i(TAG, ((TextView) view).getText().toString());
+				strikeText((TextView) view);
+				//TODO: start here; this doesn't actually work
+			}
+		});
+		
 		loadTasks();
 	}
 	
 	@Override
 	protected void onStart() {
+		//Called whenever MainActivity is resumed
 		super.onStart();		
-		assert(tasks.size() == done.size());
-		Iterator<String> tasksIt = tasks.iterator();
-		Iterator<Integer> doneIt = done.iterator();
-		taskListAdapter.clear();
-		while(tasksIt.hasNext() && doneIt.hasNext()) {
-			if(doneIt.next() == Integer.valueOf(TRUE)) {
-				tasksIt.next();
-			} else {
-				taskListAdapter.add(tasksIt.next());
-				//TextView t = new TextView(this);
-				//t.setText(tasksIt.next());
-				//t.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
-				//taskList.addView(t);
-				//TODO: make checkable task from string write
-			}
-		}
+		assert(tasks.size() == done.size());		//To make sure nothing silly happens when adding/removing tasks
+		drawTasks();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		// Inflate the menu; this adds the "add task" button to the menu bar
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		//Called when the add task menu button is pressed
+		//Starts an AddTaskActivity
 		switch (item.getItemId()) {
 			case R.id.add_task:
 				Intent addTaskIntent = new Intent(this, AddTaskActivity.class);
+				//Calls back to onActivityResult to pass data
 				startActivityForResult(addTaskIntent, ADD_TASK);
 				return true;
 			default:
@@ -80,6 +87,9 @@ public class MainActivity extends Activity {
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		//Called back from AddTaskActivity when it ends
+		//Passes a Bundle containing the task name
+		//Task is added to memory; next call in the lifecycle is onStart
 		if(resultCode == RESULT_OK) {
 			Bundle taskBundle = data.getExtras();
 			tasks.add(taskBundle.getString("task name"));
@@ -91,11 +101,33 @@ public class MainActivity extends Activity {
 	
 	@Override
 	public void onPause() {
+		//Called when this Activity is no longer in focus
+		//Calls saveTasks to store tasks in SavedPreferences
 		super.onPause();
 		saveTasks();
 	}
 	
+	private void drawTasks() {
+		//Adds tasks in memory to the list on-screen
+		//Called onStart
+		Iterator<String> tasksIt = tasks.iterator();
+		Iterator<Integer> doneIt = done.iterator();
+		taskListAdapter.clear();
+		String next;
+		while(tasksIt.hasNext() && doneIt.hasNext()) {
+			next = tasksIt.next();
+			taskListAdapter.add(next);
+			if(doneIt.next() == Integer.valueOf(TRUE)) {
+				strikeText(next);
+			} else {
+				taskListAdapter.add(tasksIt.next());
+			}
+		}
+	}
+	
 	private void saveTasks() {
+		//Saves task list and done flags to SavedPreferences
+		//Called onPause
 		SharedPreferences data = getPreferences(MODE_PRIVATE);
 		SharedPreferences.Editor editor = data.edit();
 		Iterator<String> tasksIt = tasks.iterator();
@@ -107,6 +139,8 @@ public class MainActivity extends Activity {
 	}
 	
 	private void loadTasks() {
+		//Loads task list and done flags from SavedPreferences
+		//Called onCreate
 		SharedPreferences data = getPreferences(MODE_PRIVATE);
 		Map<String, ?> dataMap = data.getAll();
 		for(Map.Entry<String, ?> entry : dataMap.entrySet()) {
@@ -114,4 +148,25 @@ public class MainActivity extends Activity {
 			done.add(Integer.valueOf(entry.getValue().toString()));
 		}
 	}
+	
+	private void strikeText(String task) {
+		//Adds strikethrough decoration to completed task
+		//Called in drawTasks
+		TextView t = (TextView) taskList.findViewById((int) taskListAdapter.getItemId(taskListAdapter.getPosition(task)));
+		t.setPaintFlags(t.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+	}
+	
+	private void strikeText(TextView t) {
+		//Adds strikethrough decoration to completed task
+		//Called onItemClick
+		t.setPaintFlags(t.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+	}
+	
+	private void unstrikeText(String task) {
+		//Removes strikethrough decoration from task
+		//TODO: Called onItemClick
+		TextView t = (TextView) taskList.findViewById((int) taskListAdapter.getItemId(taskListAdapter.getPosition(task)));
+		t.setPaintFlags(t.getPaintFlags() & Paint.STRIKE_THRU_TEXT_FLAG);
+	}
+	
 }
